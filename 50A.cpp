ac7f674a41fe24d264df5bb40ba1cd78d9a9ef64
Aluna de Ciencia da Computacao <anajuliaccomp@gmail.com>
2024-11-12 00:00:42 -0300

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+50A Domino Pilling
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{  
+    int M,N;
+    cin >> M >> N;
+    int result;
+    
+    result = (M*N)/2.0;
+    result = result;
+    cout << result;
+}
